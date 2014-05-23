5ab397c6f1a2783c2405c673aeb94381d4781c36
Steve Lacy <me@slacy.me>
2014-05-23 14:11:01 -0700

    alter gulpfile, update live-reload

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,4 @@
 var gulp = require('gulp');
-var lr = require('tiny-lr');
 var jade = require('gulp-jade');
 var wrap = require('gulp-wrap-amd');
 var coffee = require('gulp-coffee');
@@ -8,7 +7,6 @@ var reload = require('gulp-livereload');
 var stylus = require('gulp-stylus');
 require('coffee-script/register');
 
-var server = lr();
 
 gulp.task('templates', function(){
   gulp.src('./client/templates/**/*.jade')
@@ -18,7 +16,7 @@ gulp.task('templates', function(){
     params: ['jade']
     }))
   .pipe(gulp.dest('./public/templates'))
-  .pipe(reload(server));
+  .pipe(reload());
 
 });
 
@@ -26,21 +24,21 @@ gulp.task('jade', function(){
   gulp.src('./client/*.jade')
   .pipe(jade())
   .pipe(gulp.dest('./public'))
-  .pipe(reload(server));
+  .pipe(reload());
 });
 
 gulp.task('stylus', function(){
   gulp.src('./client/css/style.styl') // Note: style.styl is the root
   .pipe(stylus())
   .pipe(gulp.dest('./public/css'))
-  .pipe(reload(server));
+  .pipe(reload());
 });
 
 gulp.task('scripts',['lint:coffee'], function(){
   gulp.src('./client/js/**/*.coffee')
     .pipe(coffee())
     .pipe(gulp.dest('./public/js'))
-    .pipe(reload(server));
+    .pipe(reload());
   gulp.src('./client/js/**/*.js')
     .pipe(gulp.dest('./public/js'));
 });
@@ -54,30 +52,24 @@ gulp.task('lint:coffee', function(){
 gulp.task('copy', function(){
   gulp.src('./client/css/**/*.css')
     .pipe(gulp.dest('./public/css'))
-    .pipe(reload(server));
+    .pipe(reload());
   gulp.src('./client/images/**')
     .pipe(gulp.dest('./public/images'))
-    .pipe(reload(server));
+    .pipe(reload());
   gulp.src('./client/*.html')
     .pipe(gulp.dest('./public'))
-    .pipe(reload(server));
+    .pipe(reload());
   
 });
 
 gulp.task('watch', function(){
-  server.listen(35729, function (err) {
-    if (err) return console.log(err);
-
     gulp.watch('./client/js/**', ['scripts']);
     gulp.watch('./client/templates/**', ['templates']);
     gulp.watch('./client/*.jade', ['jade']);
     gulp.watch('./client/css/**/*.css', ['copy']);
     gulp.watch('./client/css/**/*.styl', ['stylus']);
-  
-  });
 });
 
 gulp.task('default', ['templates', 'scripts', 'copy', 'watch', 'jade', 'stylus'], function(){
-  require('./start');
-  
+  require('./start'); 
 });
